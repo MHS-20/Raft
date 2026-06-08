581852e1786684ea75b5742ae2e6ab1ce317e786
Muhamad Salman <muhamad.salman2001@gmail.com>
2026-06-08 12:47:35 +0200

    feat: added consensus model struct and base methods

---
+++ raft/raft.go
@@ -0,0 +1,97 @@
+package raft
+
+import (
+	"fmt"
+	"log"
+	"sync"
+	"time"
+)
+
+const DebugCM = 1
+
+type LogEntry struct {
+	Command any
+	Term    int
+}
+
+type CMState int
+
+const (
+	Follower CMState = iota
+	Candidate
+	Leader
+	Dead
+)
+
+func (s CMState) String() string {
+	switch s {
+	case Follower:
+		return "Follower"
+	case Candidate:
+		return "Candidate"
+	case Leader:
+		return "Leader"
+	case Dead:
+		return "Dead"
+	default:
+		panic("unreachable")
+	}
+}
+
+// ConsensusModule (CM) implements a single node of Raft consensus.
+type ConsensusModule struct {
+	mu      sync.Mutex
+	id      int // server ID of this CM
+	peerIds []int
+	server  *Server
+
+	// Persistent Raft state on all servers
+	currentTerm int
+	votedFor    int
+	log         []LogEntry
+
+	// Volatile Raft state on all servers
+	state              CMState
+	electionResetEvent time.Time
+}
+
+func NewConsensusModule(id int, peerIds []int, server *Server, ready <-chan any) *ConsensusModule {
+	cm := new(ConsensusModule)
+	cm.id = id
+	cm.peerIds = peerIds
+	cm.server = server
+	cm.state = Follower
+	cm.votedFor = -1
+
+	go func() {
+		// The CM is quiescent until ready is signaled; then, it starts a countdown
+		// for leader election.
+		<-ready
+		cm.mu.Lock()
+		cm.electionResetEvent = time.Now()
+		cm.mu.Unlock()
+		cm.runElectionTimer()
+	}()
+
+	return cm
+}
+
+func (cm *ConsensusModule) Report() (id int, term int, isLeader bool) {
+	cm.mu.Lock()
+	defer cm.mu.Unlock()
+	return cm.id, cm.currentTerm, cm.state == Leader
+}
+
+func (cm *ConsensusModule) Stop() {
+	cm.mu.Lock()
+	defer cm.mu.Unlock()
+	cm.state = Dead
+	cm.dlog("becomes Dead")
+}
+
+func (cm *ConsensusModule) dlog(format string, args ...any) {
+	if DebugCM > 0 {
+		format = fmt.Sprintf("[%d] ", cm.id) + format
+		log.Printf(format, args...)
+	}
+}
